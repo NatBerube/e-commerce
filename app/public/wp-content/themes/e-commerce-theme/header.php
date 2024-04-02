c50a5e303805aa5b064b76268d16a9bdef35a7ff
Nathaniel <nat_berube@outlook.com>
2024-04-02 14:07:34 -0400

    header-skeleton-WIP

--- app/public/wp-content/themes/e-commerce-theme/header.php
+++ app/public/wp-content/themes/e-commerce-theme/header.php
@@ -1,1 +1,21 @@
-<h1>The Header</h1>
+<div class="container">
+        <h1 class="e-commerce-logo-text float-left">
+          <a href="<?php echo site_url()?>"><?php echo get_bloginfo('name')?></a>
+        </h1>
+        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+        <div class="site-header__menu group">
+          <nav class="main-navigation">
+            <ul>              
+              <li><a href="#">Shop</a></li>
+              <li><a href="#">Contact</a></li>
+              <li><a href="<?php echo site_url('/about-us')?>">About Us</a></li>
+            </ul>
+          </nav>
+          <div class="site-header__util">
+            <a href="#" class="">Login</a>
+            <a href="#" class="">Sign Up</a>
+            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+          </div>
+        </div>
+      </div>
